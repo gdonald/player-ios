3c550e6d65cbbc50d07966bce05cde6c1a14fbc7
Greg Donald <gd@darkclear.net>
2024-01-17 19:34:37 -0600

    Initial Commit

---
+++ Player/PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerApp.swift
+//  Player
+//
+//  Created by Greg Donald on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
